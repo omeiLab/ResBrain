69e6124c075da001c61f1a33ed0778c88bfc678a
AmeHibiki <michaelmick05220806@gmail.com>
2026-05-08 01:03:04 +0800

    DB Setup

--- app/main.py
+++ app/main.py
@@ -1,3 +1,11 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from database.db import create_db_and_tables
+from models.models import Note, Paper, Tag
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
